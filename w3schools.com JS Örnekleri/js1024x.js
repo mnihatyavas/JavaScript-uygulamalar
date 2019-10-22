eba124cd66167d4be694807e1525c6ff37d9c65d
M.Nihat Yavaş <46388635+mnihatyavas@users.noreply.github.com>
2019-10-22 03:14:18 +0200

    Add files via upload

---
+++ w3schools.com JS Örnekleri/js1024x.js
@@ -0,0 +1,5 @@
+// js1024x.js Çaðrýlan fonksiyon içeren harici .js dosyasý alt-örneði.
+
+function fonksiyonum() {
+    document.getElementById ("göster").innerHTML = "Týklama olayýyla paragrafý deðiþtirdiniz.";
+} // fonksiyonum sonu...

c1aa37bc03a8d32b456571d79ceada54de17a7f5
M.Nihat Yavaş <46388635+mnihatyavas@users.noreply.github.com>
2020-07-16 22:49:20 +0300

    Add files via upload

---
+++ javascript.info JS Örnekleri/js30301a.js
@@ -0,0 +1,4 @@
+﻿// js30301a.js: Fetch ile getirilen ve işletilen js fonksiyonları alt-örneği.
+function bir() {console.log ("Birinci fonksiyon yüklendi ve işletildi");}
+function iki() {console.log ("İkinci fonksiyon yüklendi ve işletildi");}
+function üç() {console.log ("Üçüncü fonksiyon yüklendi ve işletildi");}
